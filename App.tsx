e69f97471ec013b38db544c8b28d0aa1f0bb5565
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-06-29 20:53:53 +0900

    フラグメントで実装

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const label = 'My first component';
-const isNumber = 0;
-const needsToShowDetail: boolean = true;
+import { Text } from 'react-native';
 
 export default function MyComponent() {
   return (
-    <View>
-      <Text>{label}</Text>
-      <Text>{7 + 19 + 16}</Text>
-      {false}
-      {null}
-      {undefined}
-      {isNumber ? <Text>42</Text> : <Text>foo</Text>}
-      {needsToShowDetail && <Text>式の評価結果です</Text>}
-    </View>
+    <>
+      <Text>Hello</Text>
+      <Text>World</Text>
+    </>
   );
 }
