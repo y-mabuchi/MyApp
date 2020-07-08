58ae1fe965c15e09db53c7998b2e77d451b6ac1f
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-07-08 17:51:52 +0900

    defaultProps

--- App.tsx
+++ App.tsx
@@ -1,37 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, ViewStyle } from 'react-native';
-
-interface Props {
-  children: React.ReactNode;
-  style: ViewStyle;
-  onPress: () => void;
-}
-
-function ChildComponent(props: Props) {
-  const { children, style, onPress } = props;
-
-  return (
-    <Text style={style} onPress={onPress}>
-      {children}
-    </Text>
-  );
-}
+import { Text, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  button: {
-    borderWidth: 1,
+  label: {
+    marginTop: 100,
+    padding: 8,
+    fontSize: 24,
   },
 });
 
-export default function App() {
-  return (
-    <ChildComponent
-      onPress={() => {
-        console.log('hello');
-      }}
-      style={styles.button}
-    >
-      Press Me!
-    </ChildComponent>
-  );
+interface Props {
+  backgroundColor: string;
+  color: string;
+  label: string;
 }
+
+export default function CustomLabel(props: Props) {
+  const { backgroundColor, color, label } = props;
+
+  return <Text style={[styles.label, { backgroundColor, color }]}>{label}</Text>;
+}
+
+CustomLabel.defaultProps = {
+  backgroundColor: '#008080',
+  color: 'white',
+  label: 'hello',
+};
