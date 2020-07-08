3203985ae3c3695f29c1385faef8591cdb0b2a6b
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-07-08 18:25:23 +0900

    アラートを表示

--- App.tsx
+++ App.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 100,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 32,
   },
 });
 
 export default function App() {
-  const [state, setState] = React.useState(true);
+  const [count1, setCount1] = React.useState(0);
+  const [count2, setCount2] = React.useState(0);
+
+  React.useEffect(() => {
+    Alert.alert('count1 is incremented.');
+  }, [count1, count2]);
 
   return (
-    <Text
-      style={styles.container}
-      onPress={() => {
-        setState(!state);
-      }}
-    >
-      state is {state.toString()}
-    </Text>
+    <View style={styles.container}>
+      <Text
+        onPress={() => {
+          setCount1(count1 + 1);
+        }}
+        style={styles.label}
+      >
+        {count1}
+      </Text>
+      <Text
+        onPress={() => {
+          setCount2(count2 + 1);
+        }}
+        style={styles.label}
+      >
+        {count2}
+      </Text>
+    </View>
   );
 }
