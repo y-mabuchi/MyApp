3e8730fdbdca93dc7c721e9fc46452b5e7d45a17
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-07-08 17:42:45 +0900

    ボタン追加

--- App.tsx
+++ App.tsx
@@ -1,35 +1,37 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { StyleSheet, Text, ViewStyle } from 'react-native';
+
+interface Props {
+  children: React.ReactNode;
+  style: ViewStyle;
+  onPress: () => void;
+}
+
+function ChildComponent(props: Props) {
+  const { children, style, onPress } = props;
+
+  return (
+    <Text style={style} onPress={onPress}>
+      {children}
+    </Text>
+  );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  texts: {
-    flexDirection: 'row',
-    alignItems: 'baseline',
-  },
-  text1: {
-    fontSize: 16,
-  },
-  text2: {
-    fontSize: 32,
-  },
-  text3: {
-    fontSize: 16,
+  button: {
+    borderWidth: 1,
   },
 });
 
-export default function MyComponent() {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.texts}>
-        <Text style={styles.text1}>tax</Text>
-        <Text style={styles.text2}>10</Text>
-        <Text style={styles.text3}>%</Text>
-      </View>
-    </View>
+    <ChildComponent
+      onPress={() => {
+        console.log('hello');
+      }}
+      style={styles.button}
+    >
+      Press Me!
+    </ChildComponent>
   );
 }
