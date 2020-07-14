37deb3cdc5a25beb467bd36d025556eae940e296
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-07-15 06:53:18 +0900

    スクロール

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, FlatList } from 'react-native';
+import { StyleSheet, FlatList, View, TouchableOpacity, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,66 +8,43 @@ const styles = StyleSheet.create({
   label: {
     height: 400,
   },
-  separator: {
-    height: 1,
-    backgroundColor: 'gray',
-  },
-  header: {
-    backgroundColor: 'blue',
-  },
-  footer: {
-    backgroundColor: 'green',
-  },
 });
 
-const data = [
+interface Item {
+  id: string;
+  title: string;
+}
+
+const data: Array<Item> = [
   { id: 'first', title: 'ひとつめ' },
   { id: 'second', title: 'ふたつめ' },
   { id: 'third', title: 'みっつめ' },
+  { id: 'fourth', title: 'よっつめ' },
+  { id: 'fifth', title: 'いつつめ' },
+  { id: 'sixth', title: 'むっつめ' },
+  { id: 'seventh', title: 'ななつめ' },
 ];
 
 export default function App() {
-  // const [refreshing, setRefreshing] = React.useState(false);
-  // const onRefresh = React.useCallback(() => {
-  //   try {
-  //     setRefreshing(true);
-  //     setTimeout(() => {
-  //       data.unshift({
-  //         id: new Date().getTime().toString(),
-  //         title: 'foo',
-  //       });
-  //     }, 200);
-  //   } finally {
-  //     setRefreshing(false);
-  //   }
-  // }, []);
-
-  const onEndReached = React.useCallback(() => {
-    setTimeout(() => {
-      data.push({
-        id: new Date().getTime().toString(),
-        title: 'bar',
-      });
-    }, 200);
-  }, []);
+  const list = React.useRef<FlatList<Item>>(null);
+  const scrollToThird = React.useCallback(() => {
+    list?.current?.scrollToIndex({ index: 2 });
+  }, [list]);
 
   return (
-    <FlatList
-      style={styles.container}
-      data={data}
-      renderItem={({ item }) => (
-        <Text style={styles.label} key={item.id}>
-          {item.title}
-        </Text>
-      )}
-      onEndReached={onEndReached}
-      // refreshing={refreshing}
-      // onRefresh={onRefresh}
-      // ListHeaderComponent={() => <Text style={styles.header}>Header</Text>}
-      // ListFooterComponent={() => <Text style={styles.footer}>Footer</Text>}
-      // ItemSeparatorComponent={() => <View style={styles.separator} />}
-      // horizontal
-      // numColumns={2}
-    />
+    <View style={styles.container}>
+      <TouchableOpacity onPress={scrollToThird}>
+        <Text>Scroll to third</Text>
+      </TouchableOpacity>
+      <FlatList
+        ref={list}
+        data={data}
+        renderItem={({ item }) => (
+          <Text style={styles.label} key={item.id}>
+            {item.title}
+          </Text>
+        )}
+      />
+    </View>
   );
 }
