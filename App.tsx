6fce0a1e227ed60b0d86a12b5f51d1d709689988
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-07-15 06:38:09 +0900

    データの更新

--- App.tsx
+++ App.tsx
@@ -1,35 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import { StyleSheet, Text, FlatList } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    margin: 32,
+    marginTop: 44,
   },
-  block: {
-    width: 300,
+  label: {
     height: 400,
   },
-  red: {
-    backgroundColor: 'red',
+  separator: {
+    height: 1,
+    backgroundColor: 'gray',
   },
-  green: {
-    backgroundColor: 'green',
-  },
-  blue: {
+  header: {
     backgroundColor: 'blue',
   },
-  yellow: {
-    backgroundColor: 'yellow',
+  footer: {
+    backgroundColor: 'green',
   },
 });
 
+const data = [
+  { id: 'first', title: 'ひとつめ' },
+  { id: 'second', title: 'ふたつめ' },
+  { id: 'third', title: 'みっつめ' },
+];
+
 export default function App() {
+  // const [refreshing, setRefreshing] = React.useState(false);
+  // const onRefresh = React.useCallback(() => {
+  //   try {
+  //     setRefreshing(true);
+  //     setTimeout(() => {
+  //       data.unshift({
+  //         id: new Date().getTime().toString(),
+  //         title: 'foo',
+  //       });
+  //     }, 200);
+  //   } finally {
+  //     setRefreshing(false);
+  //   }
+  // }, []);
+
+  const onEndReached = React.useCallback(() => {
+    setTimeout(() => {
+      data.push({
+        id: new Date().getTime().toString(),
+        title: 'bar',
+      });
+    }, 200);
+  }, []);
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={[styles.block, styles.red]}>RED</Text>
-      <Text style={[styles.block, styles.green]}>GREEN</Text>
-      <Text style={[styles.block, styles.blue]}>BLUE</Text>
-      <Text style={[styles.block, styles.yellow]}>YELLOW</Text>
-    </ScrollView>
+    <FlatList
+      style={styles.container}
+      data={data}
+      renderItem={({ item }) => (
+        <Text style={styles.label} key={item.id}>
+          {item.title}
+        </Text>
+      )}
+      onEndReached={onEndReached}
+      // refreshing={refreshing}
+      // onRefresh={onRefresh}
+      // ListHeaderComponent={() => <Text style={styles.header}>Header</Text>}
+      // ListFooterComponent={() => <Text style={styles.footer}>Footer</Text>}
+      // ItemSeparatorComponent={() => <View style={styles.separator} />}
+      // horizontal
+      // numColumns={2}
+    />
   );
 }
