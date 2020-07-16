8ea182777e86e96a1a483d990611013688eb6a47
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-07-17 06:13:05 +0900

    SectionList

--- App.tsx
+++ App.tsx
@@ -1,50 +1,34 @@
 import React from 'react';
-import { StyleSheet, FlatList, View, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, SectionList, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 44,
   },
   label: {
-    height: 400,
+    color: 'black',
+    backgroundColor: 'white',
+  },
+  sectionHeader: {
+    color: 'white',
+    backgroundColor: 'black',
   },
 });
 
-interface Item {
-  id: string;
-  title: string;
-}
-
-const data: Array<Item> = [
-  { id: 'first', title: 'ひとつめ' },
-  { id: 'second', title: 'ふたつめ' },
-  { id: 'third', title: 'みっつめ' },
-  { id: 'fourth', title: 'よっつめ' },
-  { id: 'fifth', title: 'いつつめ' },
-  { id: 'sixth', title: 'むっつめ' },
-  { id: 'seventh', title: 'ななつめ' },
+const sections = [
+  { title: 'basic', data: ['View', 'Text', 'Image'] },
+  { title: 'list', data: ['FlatList', 'SectionList'] },
+  { title: 'interaction', data: ['Touchable', 'TextInput'] },
 ];
 
 export default function App() {
-  const list = React.useRef<FlatList<Item>>(null);
-  const scrollToThird = React.useCallback(() => {
-    list?.current?.scrollToIndex({ index: 2 });
-  }, [list]);
-
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={scrollToThird}>
-        <Text>Scroll to third</Text>
-      </TouchableOpacity>
-      <FlatList
-        ref={list}
-        data={data}
-        renderItem={({ item }) => (
-          <Text style={styles.label} key={item.id}>
-            {item.title}
-          </Text>
-        )}
-      />
-    </View>
+    <SectionList
+      style={styles.container}
+      sections={sections}
+      renderItem={({ item }) => <Text style={styles.label}>{item}</Text>}
+      keyExtractor={(_, index) => index.toString()}
+      renderSectionHeader={({ section: { title } }) => <Text style={styles.sectionHeader}>{title}</Text>}
+    />
   );
 }
