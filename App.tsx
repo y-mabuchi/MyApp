8fa01c0aa9d54f032e452e1d7546082134389994
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-07-20 06:18:34 +0900

    StatusBarコンポーネント

--- App.tsx
+++ App.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
-import { StyleSheet, View, TextInput, Text } from 'react-native';
-import { useControlledComponent } from './lib/hooks';
+import { StatusBar, StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    borderWidth: 1,
-    width: 256,
-    padding: 4,
+    backgroundColor: 'black',
   },
 });
 
 export default function App() {
-  const familyName = useControlledComponent('');
-  const personalName = useControlledComponent('');
-
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} {...familyName} />
-      <TextInput style={styles.input} {...personalName} />
-      <Text>
-        {familyName.value} {personalName.value}
-      </Text>
+      <StatusBar barStyle="light-content" />
     </View>
   );
 }
