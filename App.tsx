29eade45fed4216759a4feb5d888821711f22c87
YusukeMabuchi <mabuchi.y@cocoro-inc.net>
2020-07-23 09:55:54 +0900

    モーダルダイアログ

--- App.tsx
+++ App.tsx
@@ -9,30 +9,43 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function App() {
-  function onPress() {
-    Alert.alert('title', 'message', [
-      {
-        text: 'OK',
-        onPress: () => {
-          console.warn('ok');
+function useErrorModalDialog(initialErrors: Array<string>) {
+  const [needsToShow, setNeedsToShow] = React.useState(true);
+  const [errors, setErrors] = React.useState(initialErrors);
+
+  React.useEffect(() => {
+    if (0 < errors.length && needsToShow) {
+      setNeedsToShow(false);
+      Alert.alert(errors[0], undefined, [
+        {
+          text: 'OK',
+          onPress: () => {
+            setErrors(errors.filter((_, index) => index !== 0));
+            setNeedsToShow(true);
+          },
         },
-        style: 'default',
-      },
-      {
-        text: 'Cancel',
-        onPress: () => {
-          console.warn('cancel');
-        },
-        style: 'cancel',
-      },
-    ]);
+      ]);
+    }
+  }, [needsToShow, errors]);
+
+  function addError(newError: string) {
+    setErrors([...errors, newError]);
   }
+
+  return addError;
+}
+
+export default function App() {
+  const addError = useErrorModalDialog(['1st', '2nd', '3rd']);
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={onPress}>
-        <Text>Alert</Text>
+      <TouchableOpacity
+        onPress={() => {
+          addError('new error');
+        }}
+      >
+        <Text>generate error</Text>
       </TouchableOpacity>
     </View>
   );
